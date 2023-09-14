fbe393e90267510686db07b34a90ebab37f909a5
jasminextan <112445202+jasminextan@users.noreply.github.com>
2023-09-14 11:41:04 -0700

    Data preprocessing 1

---
+++ Proj1_LSTM/DataPreprocess.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import tensorflow as tf
+from sklearn import MinMaxScaler, OneHotEncoder, train_test_split, RFE
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+
+minwage_df = pd.read_csv('Proj1_LSTM/MinimumWage.csv')
+partcon_df = pd.read_csv('Proj1_LSTM/MinWage_PartyControl.csv')
+
+print(minwage_df.head())
+print(partcon_df.head())
+
+#Combine the two datasets with the key 'Year'
+combined_df = pd.merge(minwage_df, partcon_df, on='Year')
+print(combined_df)
+combined_df.to_csv('Proj1_LSTM/combined_dataset.csv', index=False)
+
+# Load the dataset
+df = pd.read_csv('combined_dataset.csv')
+
+# Handle missing values
+df = df.dropna()
+
+# Perform feature scaling
+scaler = MinMaxScaler()
+numeric_features = ['FedMinWage', 'RateChange', 'PercentChange', 'YearsSinceLastChange', 'FederalMinimumWage', 'MeanAnnualInflation', 'MinWageIndexedLastRaiseYear', 'UnemploymentRateDecember', 'GDP_AnnualGrowth']
+df[numeric_features] = scaler.fit_transform(df[numeric_features])
+
+# Encode categorical variables
+categorical_features = ['PresParty', 'SenParty', 'HouseParty', 'TrifectaFlag', 'IncreaseFlag']
+encoder = OneHotEncoder(sparse=False)
+encoded_features = pd.DataFrame(encoder.fit_transform(df[categorical_features]))
+df = pd.concat([df, encoded_features], axis=1).drop(categorical_features, axis=1)
+
+# Create input sequences and target variables
+n_steps = 5  # Number of time steps in each input sequence
+X = []
+y = []
+for i in range(n_steps, len(df)):
+    X.append(df.iloc[i-n_steps:i, :].values)
+    y.append(df.iloc[i, -1])
+X = np.array(X)
+y = np.array(y)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
